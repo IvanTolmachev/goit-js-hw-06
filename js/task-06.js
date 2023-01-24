e123ce02f64054be59e5475d93b7bf7f17325f1a
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-01-24 22:21:39 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,7 @@ const inputEl = document.querySelector('#validation-input');
 // const nameAttribute = inputEl.getAttribute('data-length');
 
 inputEl.addEventListener('blur', () => {
-  if (
-    inputEl.value.length >= inputEl.dataset.length &&
-    inputEl.value.length <= inputEl.dataset.length
-  ) {
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
   } else {
